9ed5600b4de42bb2ce1ab34a9dd0fd61f1f5ac97
Benjamin <binemugha@gmail.com>
2024-07-23 08:42:52 +0100

    Added meals details screen and setup tabs

---
+++ meals_app/lib/screens/meals_details.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsDetailsScreen extends StatelessWidget {
+  const MealsDetailsScreen({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 14),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 14),
+              for (final ingredients in meal.ingredients)
+                Text(
+                  ingredients,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              const SizedBox(height: 14),
+              Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold),
+              ),
+              for (final steps in meal.steps)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    steps,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                ),
+            ],
+          ),
+        ));
+  }
+}
